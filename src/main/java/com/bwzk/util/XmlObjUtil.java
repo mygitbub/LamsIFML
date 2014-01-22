4d57f5401c9bd8b7c1f0154ac477d1c1a1cb0940
luyuwww <luyuwww@126.com>
2014-01-22 23:02:44 +0800

    加入Dom4j解析xmlString to dom4j.document的方法

--- src/main/java/com/bwzk/util/XmlObjUtil.java
+++ src/main/java/com/bwzk/util/XmlObjUtil.java
@@ -10,6 +10,10 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
 /**
  * xml和对象的互相转换.要求待转换对象是需要符合xsd描述的
  * @author LuYu
@@ -53,5 +57,33 @@ public class XmlObjUtil<T> {
 			}
 		}
 		return returnXml;
+	}
+	
+	/**
+	 * <p>Title: xmlString parse to dom4j.document</p>
+	 * <p>Description: 利用dom4j 将xmlstr解析成dom4j.Document</p>
+	 * @param xmlStr
+	 * @return: dom4j.Document
+	 * 
+	 * @date 2014年1月22日
+	*/
+	public static synchronized Document xmlStr2Document(String xmlStr){
+		Document docu = null;
+		SAXBuilder sBuilder = null;
+		StringReader reader = null;
+		try {
+			sBuilder = new SAXBuilder();
+			reader = new StringReader(xmlStr);
+			docu = sBuilder.build(reader);
+		} catch (JDOMException e) {
+			throw new RuntimeException("xml字符串为空"+e.getMessage());
+		} catch (IOException e) {
+			throw new RuntimeException("xml字符串为空"+e.getMessage());
+		}finally{
+			if(reader != null){
+				reader.close();
+			}
+		}
+		return docu;
 	}
 }
